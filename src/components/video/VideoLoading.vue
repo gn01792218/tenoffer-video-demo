b2491366acc54b8285ec089ed1aa1169f30aa38f
gn01792218 <gn01792218@hotmail.com>
2022-07-06 10:24:42 +0800

    新增Video Loading

--- src/components/video/VideoLoading.vue
+++ src/components/video/VideoLoading.vue
@@ -1,5 +1,5 @@
 <template>
-    <div class="video-loading">
+    <div class="w-[200px] h-[200px] bg-[url('src/assets/images/videoLoading.svg')] absolute left-1/2 top-1/2 translate-x-[-50%] translate-y-[-50%]">
     </div>
 </template>
 
