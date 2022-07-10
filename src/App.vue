bdff11be6168d2020e422c42786a177721db4107
MSI\gn01792218 <gn01792218@hotmail.com>
2022-07-10 11:30:37 +0800

    新增關閉網頁和重整網頁時導回首頁機制

--- src/App.vue
+++ src/App.vue
@@ -2,5 +2,29 @@
   <router-view/>
 </template>
 <script setup lang="ts">
+import { ref } from 'vue'
+import { onMounted } from '@vue/runtime-core'
+let beforeUnloadTime = ref(0)
+let gapTime = ref(0)
 
+const beforeunloadHandler = () => {
+  console.log('刷新')
+  localStorage.removeItem("token")
+  beforeUnloadTime.value = new Date().getTime();
+}
+
+const unloadHandler = () => {
+  gapTime.value = new Date().getTime() - beforeUnloadTime.value;
+  //判断是窗口关闭还是刷新
+  if (gapTime.value <= 5) {//浏览器关闭
+    localStorage.removeItem("token")
+  }
+}
+
+onMounted(() => {
+  //监听页面卸载（关闭）或刷新
+  window.addEventListener('beforeunload', () => beforeunloadHandler())
+
+  window.addEventListener('unload', () => unloadHandler())
+})
 </script>
--- src/views/VideoDemo.vue
+++ src/views/VideoDemo.vue
@@ -7,14 +7,4 @@
 <script setup lang="ts">
 import LiveVideo from "@/components/video/LiveVideo.vue";
 import Header from '@/views/Header.vue'
-import { onMounted } from "@vue/runtime-core";
-import useRoute  from "@/composable/util/useRouter";
-onMounted(()=>{
-    const { routerPush } = useRoute()
-    let userToken = localStorage.getItem('token')
-    if(!userToken){
-        
-    }
-})
-
 </script>
